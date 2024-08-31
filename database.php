93042e41b90114d8bcd9fe744e4a651985996666
Mohammad Shayan <officialm.shayan@gmail.com>
2024-08-31 16:17:56 +0500

    finally peace

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=mshayan_telinksimg_db', 'mshayan_telinksimg_db', '@Shayan@786');
+$pdo = new PDO('mysql:host=localhost;dbname=mshayan_telinksimg_db', 'mshayan_telinksimg_db', 'Q^G05I9i}H_W');
 // $pdo = new PDO('mysql:host=localhost;dbname=telinksimg_db', 'root', '');
 ?>
