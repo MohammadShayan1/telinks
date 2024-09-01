7f32470dc1a8d83a7387d8d1f6e77e38f05550f0
Mohammad Shayan <officialm.shayan@gmail.com>
2024-09-02 02:17:33 +0500

    email unsubscribe changes

--- assets/forms/unsubscribepage.php
+++ assets/forms/unsubscribepage.php
@@ -1,4 +1,6 @@
 <?php
+include ('../../header.php');
+include ('unsubscribe.php');
 if (isset($_SESSION['success_message'])) {
     echo $_SESSION['success_message'];
     unset($_SESSION['success_message']);
@@ -7,4 +9,5 @@ if (isset($_SESSION['error_message'])) {
     echo $_SESSION['error_message'];
     unset($_SESSION['error_message']);
 }
+include ('../../footer.php');
 ?>
